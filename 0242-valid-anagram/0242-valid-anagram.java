108518db57bf742daeff541187d1231a94c4549c
Tejasvi Jain <141146403+tejasvijain09@users.noreply.github.com>
2026-01-25 23:55:22 +0530

    Time: 15 ms (24.84%), Space: 46.5 MB (38.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Character,Integer> freq = new HashMap<>();
 
-        if(s.length() != t.length()) return false;
-        for(char c : s.toCharArray()){
-            freq.put(c,freq.getOrDefault(c,0) + 1);
-        }
-        for(int j=0;j<t.length();j++){
-            char ch = t.charAt(j);
-            if(!freq.containsKey(ch)) return false; // stuck point 
-            freq.put(ch,freq.get(ch) - 1);
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map1.put(c, map1.getOrDefault(c, 0) + 1);
         }
 
-        for(int value: freq.values()){
-            if(Math.abs(value) > 0) return false;
+        for (char c : t.toCharArray()) {
+            map2.put(c, map2.getOrDefault(c, 0) + 1);
         }
-        return true;
+
+        return map1.equals(map2);
     }
 }
