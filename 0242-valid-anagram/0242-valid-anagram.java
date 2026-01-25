cb50c56ac751999174da742f98a6f41e58dd9f8f
Tejasvi Jain <141146403+tejasvijain09@users.noreply.github.com>
2026-01-25 23:56:56 +0530

    Time: 3 ms (90.52%), Space: 46.4 MB (38.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,19 +1,13 @@
-class Solution {
+public class Solution {
     public boolean isAnagram(String s, String t) {
-
-        if (s.length() != t.length()) return false;
-
-        Map<Character, Integer> map1 = new HashMap<>();
-        Map<Character, Integer> map2 = new HashMap<>();
-
-        for (char c : s.toCharArray()) {
-            map1.put(c, map1.getOrDefault(c, 0) + 1);
+        if (s.length() != t.length()) {
+            return false;
         }
 
-        for (char c : t.toCharArray()) {
-            map2.put(c, map2.getOrDefault(c, 0) + 1);
-        }
-
-        return map1.equals(map2);
+        char[] sSort = s.toCharArray();
+        char[] tSort = t.toCharArray();
+        Arrays.sort(sSort);
+        Arrays.sort(tSort);
+        return Arrays.equals(sSort, tSort);
     }
 }
