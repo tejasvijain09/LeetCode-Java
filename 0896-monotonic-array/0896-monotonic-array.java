50c703f5068a47965d6579f7a9428d9d6abc4045
Tejasvi Jain <141146403+tejasvijain09@users.noreply.github.com>
2026-01-27 13:58:23 +0530

    Time: 1 ms (95.3%), Space: 85.1 MB (73.58%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,12 +1,42 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        boolean inc = false, dec = false;
 
+        // 0 = direction not decided yet
+        // 1 = increasing
+        // -1 = decreasing
+        int direction = 0;
+
+        // compare each element with the next one
         for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] < nums[i + 1]) inc = true;
-            if (nums[i] > nums[i + 1]) dec = true;
-            if (inc && dec) return false;
+
+            // if current number is smaller than next → increasing
+            if (nums[i] < nums[i + 1]) {
+
+                // if we already decided it should be decreasing, it's invalid
+                if (direction == -1) {
+                    return false;
+                }
+
+                // mark direction as increasing
+                direction = 1;
+            }
+
+            // if current number is greater than next → decreasing
+            else if (nums[i] > nums[i + 1]) {
+
+                // if we already decided it should be increasing, it's invalid
+                if (direction == 1) {
+                    return false;
+                }
+
+                // mark direction as decreasing
+                direction = -1;
+            }
+
+            // if nums[i] == nums[i + 1], do nothing (equal values are allowed)
         }
+
+        // if no contradiction found, array is monotonic
         return true;
     }
 }
