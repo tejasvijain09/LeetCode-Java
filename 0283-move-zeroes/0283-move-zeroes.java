357ffde78a6de1b1f852f756c5dcdac2a7fb2b63
Tejasvi Jain <141146403+tejasvijain09@users.noreply.github.com>
2026-01-28 10:43:33 +0530

    Time: 2 ms (89.97%), Space: 47.9 MB (29.4%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,12 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left = 0; // position of 0 to be swapped 
+        int left = 0;
 
         for (int right = 0; right < nums.length; right++) {
-            if (nums[right] != 0) {
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
+            if (nums[right] != 0) { //  skips writes when the non-zero element is already in the correct position.
+                if (left != right) {
+                    nums[left] = nums[right];
+                    nums[right] = 0;
+                }
                 left++;
             }
         }
