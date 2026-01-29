fa3a442da4685dca4036d05bcbe56601b2ba6130
Tejasvi Jain <141146403+tejasvijain09@users.noreply.github.com>
2026-01-29 19:00:58 +0530

    Time: 24 ms (26.27%), Space: 95 MB (25.2%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-       Arrays.sort(nums);
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) return true;
-            }
-        return false;
+       Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+        map.put(num, map.getOrDefault(num, 0) + 1);
+        if (map.get(num) > 1) return true;
+    }
+    return false;
     }
 }
